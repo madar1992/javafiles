4f6d6837bf6fd546ac2910390d60c36cbdc036f4
madar1992 <96854154+madar1992@users.noreply.github.com>
2022-03-07 12:55:18 +0530

    Create Zero.java

---
+++ Zero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Zero
+{
+    public static void main(String args[])
+    {
+        int number;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number you want to check:");
+        number=sc.nextInt();
+        if(number>0)
+            System.out.println(number+" is positive number");
+        else if(number<0)
+            System.out.println(number+" is negative number");
+        else
+            System.out.println(" it's a zero");        
+    }
+}
